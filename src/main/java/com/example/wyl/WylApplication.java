9e1f6cfbd0a632e9ead92ffed738de7257c9d415
wangyuliang <2589475332@qq.com>
2018-10-17 19:01:20 +0800

    Initial commit

---
+++ src/main/java/com/example/wyl/WylApplication.java
@@ -0,0 +1,12 @@
+package com.example.wyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WylApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WylApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/wyl/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.example.wyl.mapper;
+
+import com.example.wyl.bean.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
